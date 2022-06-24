2226186d4c4389bca14f22cdfde969a1964e2d5c
tankapf <107043192+tankapf@users.noreply.github.com>
2022-06-24 14:13:56 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-#Bəzi Pylardaki sözləin hamısını oxu sora götür repoları))>
+#SDBOTs <https://t.me/SDBOTs_Inifinity>
 
 import os
-API_ID = int(os.getenv("19832689"))
-API_HASH = os.getenv("a35f2c0d6c4d25456cd01dbe3547f5de")
-BOT_TOKEN = os.getenv("5451394944:AAGMpjdK3QE7vYlmK667kOTg-MsNH_kf1B0")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
