7511f0ec190ed2ff2d2c80c65a0ee884907eae40
tankapf <107043192+tankapf@users.noreply.github.com>
2022-06-24 13:38:33 +0400

    Update __main__.py

--- SDSongBot/__main__.py
+++ SDSongBot/__main__.py
@@ -1,4 +1,4 @@
-#SDBOTs <https://t.me/SDBOTs_Inifinity>
+#Reponu Öz Adına Çıxaran Bir Başa @Vusaliw'a Ata Desin!
 
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from SDSongBot.plugins import *
